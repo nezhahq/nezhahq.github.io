f3a29fbf7c5db2d4a1b17c0dc37e567d9fd4e104
naiba <naiba@users.noreply.github.com>
2022-07-21 05:36:14 +0000

    deploy: 4aa07d2e10d76e74b8bf8b2d1e72b25850c658b6

---
+++ assets/en_US_guide_q6.md.19c6b368.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,a}from"./app.4f7c959e.js";const m='{"title":"How to set up a monthly reset of transfer statistics?","description":"","frontmatter":{},"headers":[],"relativePath":"en_US/guide/q6.md","lastUpdated":1658381742000}',o={},i=a("",4),r=[i];function n(c,l,h,d,f,p){return s(),e("div",null,r)}var u=t(o,[["render",n]]);export{m as __pageData,u as default};
