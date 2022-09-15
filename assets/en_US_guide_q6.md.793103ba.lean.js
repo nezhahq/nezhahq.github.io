637011cff24d3f07c16b7327fc09be6d00492cd3
hhhkkk520 <hhhkkk520@users.noreply.github.com>
2022-09-15 16:03:58 +0000

    deploy: 37148a77ab467df567bfe4ebed194a5106acd122

---
+++ assets/en_US_guide_q6.md.793103ba.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,a}from"./app.d87a5962.js";const m='{"title":"How to set up a monthly reset of transfer statistics?","description":"","frontmatter":{},"headers":[],"relativePath":"en_US/guide/q6.md","lastUpdated":1663257347000}',o={},i=a("",4),r=[i];function n(c,l,h,d,f,p){return s(),e("div",null,r)}var u=t(o,[["render",n]]);export{m as __pageData,u as default};
