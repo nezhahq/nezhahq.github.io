d12abefd873d76fae6f36c7e0c737d8af403668e
Theo <fanxiaobin422@gmail.com>
2024-07-07 21:56:07 +0800

    修改样式增加翻译

--- docs/.vitepress/theme/components/DocAsideannouncement.vue
+++ docs/.vitepress/theme/components/DocAsideannouncement.vue
@@ -1,72 +1,83 @@
-<template>
-    <div>
-        <a v-for="(banner, index) in banners" :key="index" :href="banner.link" :name="banner.name" :title="banner.name"
-            class="banner" target="_blank">
-            <img v-if="banner.icon" :src="banner.icon" width="22" height="22" />
-            <span>
-                <p v-if="banner.name" class="name">{{ banner.name }}</p>
-                <p v-if="banner.info1" class="info">{{ banner.info1 }}</p>
-                <p v-if="banner.info2" class="info">{{ banner.info2 }}</p>
-                <p v-if="banner.version" class="version">最新版本: {{ banner.latestVersion }}</p>
-            </span>
-        </a>
-    </div>
-</template>
-
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
+import { useData } from 'vitepress';
 
 export default defineComponent({
     name: 'BannerComponent',
     setup() {
-        const banners = ref([
-            {
-                link: 'https://t.me/nezhanews',
-                icon: 'https://nezha.wiki/logo.png',
-                name: '哪吒监控公告板',
-                info1: 'Telegram Channel',
-                version: true,
-                // latestVersion: 'v0.0.0',
-            },
-            // {
-            //     link: 'https://t.me/nezhamonitoring_global',
-            //     icon: 'https://nezha.wiki/logo.png',
-            //     name: 'Nezha Monitoring Global',
-            //     // info1: '哪吒监控（英文群组）',
+        const { site } = useData();
 
-            // },
-            // {
-            //     link: 'https://t.me/nezhamonitoring',
-            //     icon: 'https://nezha.wiki/logo.png',
-            //     name: '哪吒监控（中文群组）',
-            // },
-        ]);
+        const banner = ref({
+            link: 'https://t.me/nezhanews',
+            icon: 'https://nezha.wiki/logo.png',
+            dashboardVersionApi: 'https://api.github.com/repos/naiba/nezha/releases/latest',
+            agentVersionApi: 'https://api.github.com/repos/nezhahq/agent/releases/latest',
+            dashboardVersion: '',
+            agentVersion: '',
+        });
 
         onMounted(async () => {
             await fetchLatestVersions();
         });
 
         const fetchLatestVersions = async () => {
-            for (let banner of banners.value) {
-                if (banner.version) {
-                    try {
-                        const response = await fetch('https://api.github.com/repos/naiba/nezha/releases/latest');
-                        const data = await response.json();
-                        banner.latestVersion = data.tag_name;
-                    } catch (error) {
-                        console.error('Error fetching latest version:', error);
-                    }
-                }
+            try {
+                const dashboardResponse = await fetch(banner.value.dashboardVersionApi);
+                const dashboardData = await dashboardResponse.json();
+                banner.value.dashboardVersion = dashboardData.tag_name;
+            } catch (error) {
+                console.error('Error fetching dashboard latest version:', error);
+                banner.value.dashboardVersion = 'Error fetching';
+            }
+
+            try {
+                const agentResponse = await fetch(banner.value.agentVersionApi);
+                const agentData = await agentResponse.json();
+                banner.value.agentVersion = agentData.tag_name;
+            } catch (error) {
+                console.error('Error fetching agent latest version:', error);
+                banner.value.agentVersion = 'Error fetching';
             }
         };
 
+        const translations = {
+            name: {
+                'en-US': 'Announcement',
+                'zh-CN': '公告栏',
+            },
+            version: {
+                'en-US': 'Latest Version',
+                'zh-CN': '最新版本',
+            },
+            change: {
+                'en-US': 'View changelog',
+                'zh-CN': '查看变更日志',
+            },
+        };
+
         return {
-            banners,
-            fetchLatestVersions
+            banner,
+            translations,
+            site,
+            fetchLatestVersions,
         };
     },
 });
 </script>
+
+<template>
+    <div>
+        <a :href="banner.link" class="banner" target="_blank">
+            <img v-if="banner.icon" :src="banner.icon" width="22" height="22" />
+            <!-- <span class="name">{{ translations.name[site.lang] }}</span> -->
+            <div class="info">
+                <p v-if="banner.dashboardVersion !== ''" class="version">Dashboard {{ banner.dashboardVersion }}</p>
+                <p v-if="banner.agentVersion !== ''" class="version">Agent {{ banner.agentVersion }}</p>
+                <p class="change">{{ translations.change[site.lang] }}</p>
+            </div>
+        </a>
+    </div>
+</template>
 
 <style lang="scss" scoped>
 .VPDocAsideSponsors {
@@ -78,16 +89,15 @@ export default defineComponent({
     padding: 0.4rem 0;
     border-radius: 0.8rem;
     position: relative;
-    font-size: 0.9rem;
-    font-weight: 700;
-    line-height: 1.1rem;
     display: flex;
+    align-items: center;
+    // height: 5rem;    
     align-items: center;
     justify-content: center;
     width: 100%;
     gap: 1rem;
-    background-color: var(--vp-c-bg-alt);
-    border: 2px solid var(--vp-c-bg-alt);
+    background-color: var(--vp-c-bg-soft);
+    border: 2px solid var(--vp-c-bg-soft);
     transition: border-color 0.5s;
 
     &:last-of-type {
@@ -107,30 +117,43 @@ export default defineComponent({
         transform: scale(1.75);
     }
 
-
-    .info {
-        color: var(--vp-c-text-2);
-        font-size: 0.7rem;
-        padding-left: 0.1rem;
-    }
-
     .name {
         background-image: linear-gradient(120deg, var(--vp-c-brand-3) 32%, var(--vp-c-brand-2), var(--vp-c-brand-1));
         background-clip: text;
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
+        font-size: 0.9rem;
+        font-weight: 600;
+        margin-left: 1rem;
+        writing-mode: vertical-rl;
+        text-orientation: upright;
+    }
+
+    .info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 0.5;
+        height: 100%;
+        position: relative;
     }
 
     .version {
         color: var(--vp-c-text-2);
-        font-size: 0.7rem;
-        padding-left: 0.1rem;
+        font-size: 0.8rem;
+        font-weight: 700;
+        white-space: nowrap;
+        margin-top: auto;
+        margin-bottom: auto;
     }
 
-    &:hover {
-        .info {
-            opacity: 0.9;
-        }
+    .change {
+        color: var(--vp-c-text-1);
+        font-size: 0.8rem;
+        font-weight: 700;
+        margin-top: auto;
+        text-align: center;
     }
 }
 </style>
